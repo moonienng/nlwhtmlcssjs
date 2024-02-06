7a3f585f6745c12eeec6a8e37c194ee48180049f
gabriella <128649836+moonienng@users.noreply.github.com>
2024-02-06 16:35:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -92,6 +92,11 @@ const perguntas = [
 ];
 const quiz = document.querySelector('#quiz')
 const template = document.querySelector('template')
+const corretas = new Set()
+const totalDePerguntas = perguntas.length
+
+const mostrartTotal = document.querySelector('#acertos span')
+mostrartTotal.textContent = corretas.size + ' de ' + totalDePerguntas
 for (const item of perguntas)
 {
     
@@ -102,6 +107,17 @@ for(let resposta of item.respostas)
 {
     const dt = quizItem.querySelector('dl dt').cloneNode(true)
     dt.querySelector('span').textContent = resposta
+    dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
+    dt.querySelector('input').value = item.respostas.indexOf(resposta)
+
+    dt.querySelector('input').onchange = (event) => {
+        const estaCorreta = event.target.value == item.correta //boleano
+        corretas.delete(item)
+        if(estaCorreta){
+            corretas.add(item)
+        }
+        mostrartTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+    }
 
     quizItem.querySelector('dl').appendChild(dt)
 }
